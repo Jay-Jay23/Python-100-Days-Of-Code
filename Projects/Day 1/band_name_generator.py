5452343ccca93d646c1e51300f3c222b24375d5c
Jojo <134422850+Jay-Jay23@users.noreply.github.com>
2023-05-31 13:22:19 +0200

    Create band_name_generator.py

---
+++ Projects/Day 1/band_name_generator.py
@@ -0,0 +1,7 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Day 1 of #100DayOfCode")
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
