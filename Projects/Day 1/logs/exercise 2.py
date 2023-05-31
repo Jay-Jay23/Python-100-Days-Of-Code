e952b768d06b589defdeeae44cdc24975a89ced1
Jojo <134422850+Jay-Jay23@users.noreply.github.com>
2023-05-31 13:44:46 +0200

    Create exercise 2.py

---
+++ Projects/Day 1/logs/exercise 2.py
@@ -0,0 +1,8 @@
+# Missing double quotes before word "Day".
+print("Day 1 - String Manipulation")
+# Add backslash to escape double quotes within the string or replace outer double quotes with single quotes.
+print("String Concatenation is done with the \"+\" sign.")
+# Remove extra indentation
+print('e.g. print("Hello " + "world")')
+# Extra "(" in the beginning of print function
+print("New lines can be created with a backslash and n.")
